2049e1c9368aefb8bffb51b88db835ba964ae527
Miguel de la Cruz <mgdelacroix@gmail.com>
2013-12-20 08:43:03 +0100

    More mc bindings

--- bindings.el
+++ bindings.el
@@ -19,5 +19,7 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-M->") 'mc/skip-to-next-like-this)
+(global-set-key (kbd "C-M-<") 'mc/skip-to-previous-like-this)
 
 (provide 'bindings)
